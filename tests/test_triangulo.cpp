8b99a7756115e4b976e8bf0577a2215a84bd4ab9
ChristopherCalderon <62729165+ChristopherCalderon@users.noreply.github.com>
2025-03-02 00:01:48 -0600

    Update test_triangulo.cpp

--- tests/test_triangulo.cpp
+++ tests/test_triangulo.cpp
@@ -1,1 +1,33 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include "../triangulo.cpp"
 
+TEST(TrianguloTest, OutputCorrecto) {
+    std::stringstream input("2\n4\n5\n");
+    std::stringstream output;
+    std::streambuf* cin_backup = std::cin.rdbuf();
+    std::streambuf* cout_backup = std::cout.rdbuf();
+    
+    std::cin.rdbuf(input.rdbuf());
+    std::cout.rdbuf(output.rdbuf());
+
+    main();  // Llamamos a la función principal del estudiante
+
+    std::cin.rdbuf(cin_backup);
+    std::cout.rdbuf(cout_backup);
+
+    std::string expected = 
+        "*\n"
+        "**\n"
+        "*\n"
+        "**\n"
+        "***\n"
+        "****\n"
+        "*\n"
+        "**\n"
+        "***\n"
+        "****\n"
+        "*****\n";
+
+    EXPECT_EQ(output.str(), expected);
+}
